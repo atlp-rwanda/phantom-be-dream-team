68379d18c2f2a38e73c651044149b5b6256ccc69
Rukundo725 <gerukundo14@gmail.com>
2022-06-13 14:47:14 +0200

    confugured postgress with sequelizer ORM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import app from './app.js';
-const PORT =3000;
-app.listen(PORT, () => { console.log("Server listening on port " + PORT) });
+import express from 'express';
+import route from './routes/index'
+
+// const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+route(app);
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('App is now running at port ', port)
+})
