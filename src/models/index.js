6ea98a405e3ae5722b42bd3725176f6f7fc1da2e
Rukundo725 <gerukundo14@gmail.com>
2022-06-14 10:22:16 +0200

    updated scripts in package.json

--- src/models/index.js
+++ src/models/index.js
@@ -19,7 +19,6 @@ fs
   .readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach(file => {
-    // eslint-disable-next-line global-require,import/no-dynamic-require
     const model = require(path.join(__dirname, file)).default(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
--- src/models/user.js
+++ src/models/user.js
@@ -5,21 +5,13 @@ const PROTECTED_ATTRIBUTES = ['password'];
 export default (sequelize, DataTypes) => {
   class User extends Model {
     toJSON() {
-      // hide protected fields
       const attributes = { ...this.get() };
-      // eslint-disable-next-line no-restricted-syntax
       for (const a of PROTECTED_ATTRIBUTES) {
         delete attributes[a];
       }
       return attributes;
     }
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
   };
   User.init({
