49cebaed658278d8cae1a37b38ebc2389af2bd78
Rukundo725 <gerukundo14@gmail.com>
2022-06-14 10:25:28 +0200

    removed unnessecary codes from config.js file

--- src/config/config.js
+++ src/config/config.js
@@ -1,28 +1,3 @@
-// {
-//   "development": {
-//     "username": "postgres",
-//     "password": "test",
-//     "database": "phantom",
-//     "port": "5432",
-//     "host": "127.0.0.1",
-//     "dialect": "postgres"
-
-//  },
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "test"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "postgres"
-//   }
-// }
 require('dotenv').config()
 
 module.exports = {
