9c422b73e52674d2ffd7beb341cc879e374a905a
arthur <you@example.com>
2022-06-22 08:19:57 +0200

    edited .env.sample

--- src/config/config.js
+++ src/config/config.js
@@ -5,19 +5,19 @@ dotenv.config();
 module.exports = {
   development: {
     JWT_SECRET: process.env.JWT_SECRET,
-    url: process.env.DEV_URL,
+    url: process.env.DB_DEVELOPMENT_URL,
     dialect: 'postgres',
     port: process.env.PORT,
   },
   test: {
     JWT_SECRET: process.env.JWT_SECRET,
-    url: process.env.TEST_URL,
+    url: process.env.DB_TESTING_URL,
     dialect: 'postgres',
     port: process.env.PORT,
   },
   production: {
     JWT_SECRET: process.env.JWT_SECRET,
-    url: process.env.PROD_URL,
+    url: process.env.DB_PRODUCTION_URL,
     dialect: 'postgres',
     port: process.env.PORT,
   },
