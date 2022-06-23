ffd1db8587df6e83bbea9b5cf8e0c742aa25b01c
arthur <you@example.com>
2022-06-23 14:53:27 +0200

    ft(crud):crud opeartion for routes

--- src/controllers/routeController.js
+++ src/controllers/routeController.js
@@ -54,7 +54,7 @@ const updateRoute = async (req, res) => {
   const id = req.params.id;
   let latitude = req.body.latitude;
   let longitude = req.body.longitude;
-  await busRoutes.findOne({where: {routeSlug: id}})
+  await busRoutes.findOne({where: {routeId: id}})
       .then((data) => {
         if (data != null) {
                 // eslint-disable-next-line max-len
