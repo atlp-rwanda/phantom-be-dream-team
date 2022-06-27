a5efeabe2e87c63c63e0acb5833023a96436d3e9
Ben Africa <byiringiro5ben10@gmail.com>
2022-06-27 15:19:01 +0200

    fix tests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
+
 import app from './app';
-import config from './config/config';
-
-const currentConfig = config[process.env.NODE_ENV];
-const {port} = currentConfig;
-
-const App = app.listen(port, () =>
-  console.log(`App listening on ${port}!....`),
-);
-
-export default App;
+import 'dotenv/config';
+const PORT = process.env.PORT;
+app.listen(PORT, () => { console.log("Server listening on port " + PORT) }); 	
+export default app;
