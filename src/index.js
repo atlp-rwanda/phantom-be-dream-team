08e474e229568c5c8f11c042f3b62179956346f9
shemalucien <shemalucien5@gmail.com>
2022-06-27 15:11:08 +0200

    feature(assign): assign bus to routes
    add models
    add bus to route assignment
    add unassign bus to route
    add tests
    add documentation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import app from './app';
 import 'dotenv/config';
 const PORT = process.env.PORT;
 app.listen(PORT, () => { console.log("Server listening on port " + PORT) }); 	
-
 export default app;
