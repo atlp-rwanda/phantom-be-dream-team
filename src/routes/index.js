43682ee7d5f3c52fb808db66e64c113b64579e72
stevenkwizera06 <kwiste06@gmail.com>
2022-06-28 09:25:15 +0200

    solved conflicts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,15 @@
 import express from 'express';
 import userRoutes from './api/userRoutes';
-<<<<<<< HEAD
 import busRoute from './api/busRoute'
-=======
 import Profilerouter from './api/updateProfile';
 import simulateRouter from './api/busInRoad';
->>>>>>> origin
 
 // eslint-disable-next-line new-cap
 const routes = express.Router();
 
 routes.use('/users', userRoutes);
-<<<<<<< HEAD
 routes.use('/buses', busRoute)
-=======
 routes.use('/profile',Profilerouter);
 routes.use('/simulate',simulateRouter);
->>>>>>> origin
 
 export default routes;
