75f3876af4aa88a180d9e30171db28a476062562
Rukundo725 <gerukundo14@gmail.com>
2022-06-29 16:05:57 +0200

    created users table

---
+++ src/models/user.js
@@ -0,0 +1,41 @@
+import {Model} from 'sequelize';
+
+module.exports = (sequelize, DataTypes) => {
+  // eslint-disable-next-line require-jsdoc
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate() {
+      // define association here
+    }
+  }
+  User.init(
+      {
+        firstName: {type: DataTypes.STRING, allowNull: false},
+        lastName: {type: DataTypes.STRING, allowNull: false},
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {isEmail: true},
+          lowercase: true,
+        },
+        role: {
+          type: DataTypes.STRING, allowNull: false},
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {min: 4},
+        },
+      },
+      {
+        sequelize,
+        modelName: 'User',
+      },
+  );
+
+  return User;
+};
