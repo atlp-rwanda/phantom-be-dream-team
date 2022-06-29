0e2c0ac1460d324159d2261bff5313e769dafa82
Rukundo725 <gerukundo14@gmail.com>
2022-06-29 11:38:46 +0200

    updated files

--- src/config/config.js
+++ src/config/config.js
@@ -1,28 +1,3 @@
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// module.exports = {
-//   development: {
-//     JWT_SECRET: process.env.JWT_SECRET,
-//     url: process.env.DB_DEVELOPMENT_URL,
-//     dialect: 'postgres',
-//     port: process.env.PORT,
-//   },
-//   test: {
-//     JWT_SECRET: process.env.JWT_SECRET,
-//     url: process.env.DB_TESTING_URL,
-//     dialect: 'postgres',
-//     port: process.env.PORT,
-//   },
-//   production: {
-//     JWT_SECRET: process.env.JWT_SECRET,
-//     url: process.env.DB_PRODUCTION_URL,
-//     dialect: 'postgres',
-//     port: process.env.PORT,
-//   },
-// };
-
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -30,7 +5,7 @@ dotenv.config();
 module.exports = {
   development: {
     JWT_SECRET: process.env.JWT_SECRET,
-    url: "postgres://postgres:12345@localhost:8000/phantom_db",
+    url:process.env.DB_DEVELOPMENT_URL,
     dialect: 'postgres',
     port: process.env.DB_PORT,
   },
