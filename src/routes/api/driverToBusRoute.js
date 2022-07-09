a60fbb07c3528d13c8be2b84b03f0c903e3483ca
Rukundo725 <gerukundo14@gmail.com>
2022-07-09 15:04:22 +0200

    protected the assign drivers to buses route

--- src/routes/api/driverToBusRoute.js
+++ src/routes/api/driverToBusRoute.js
@@ -1,27 +1,28 @@
 import express from 'express';
 import {AssignDriverToBus,unAssignDriverToBus,AllAssignedBuses,AllAssignedDrivers,} from '../../controllers/driverToBusController';
 import models from "../../models"
+import { checkAdmin } from "../../middleware/check";
 import {paginatedResult} from "../../middleware/driverToBusPagination"
 
 const router = express.Router();
 
 
   router.post(
-    '/assign/driver/:driverId/bus/:busId',
+    '/assign/driver/:driverId/bus/:busId',checkAdmin,
     AssignDriverToBus
   );
 
   router.get(
-    '/all/assigned/buses', paginatedResult(models.Bus),
+    '/all/assigned/buses',checkAdmin, paginatedResult(models.Bus),
     AllAssignedBuses
   );
   router.get(
-    '/all/assigned/drivers',
+    '/all/assigned/drivers',checkAdmin,
     AllAssignedDrivers,
   ); 
 
   router.put(
-    '/unassign/driver/:driverId/bus/:busId',
+    '/unassign/driver/:driverId/bus/:busId',checkAdmin,
     unAssignDriverToBus
   );
  
