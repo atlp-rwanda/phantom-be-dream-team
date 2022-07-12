bbfb441d61e340b99e83d99a4ed67b1581cfead5
Rukundo725 <gerukundo14@gmail.com>
2022-07-12 18:49:08 +0200

    updated bus to route routes and protected this route

--- src/routes/api/busToRoute_Routes.js
+++ src/routes/api/busToRoute_Routes.js
@@ -1,30 +1,30 @@
 import express from 'express';
 import {AssignBusToRoute,AllBusesWithRoutes,AllBusesWithNoRoutes,unAssignBusToRoute} from '../../controllers/AssignBustoRouteController'
-// import models from "../../models"
-// import { checkAdmin } from "../../middleware/check";
-// import {paginatedResult} from "../../middleware/driverToBusPagination"
+import models from "../../models"
+import { checkAdmin } from "../../middleware/check";
+import {paginatedResult} from "../../middleware/busToRoutePagination"
 
 const router = express.Router();
 
 
   router.post(
-    '/assign/bus/:busId/route/:routeId',
+    '/assign/bus/:busId/route/:routeId',checkAdmin,
     AssignBusToRoute
   );
 
   router.get(
-    '/all/assigned/buses',
+    '/all/assigned/buses',checkAdmin, paginatedResult(models.Bus),
     AllBusesWithRoutes
   );
   router.get(
-    '/all/unassigned/buses',
+    '/all/unassigned/buses',checkAdmin,
     AllBusesWithNoRoutes
   );
   router.put(
-    '/unassign/bus/:busId/route/:routeId',
+    '/unassign/bus/:busId/route/:routeId',checkAdmin,
     unAssignBusToRoute
   ); 
-  
+   
  
  
  
