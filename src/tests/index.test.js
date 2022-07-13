695a8f9752b644a06b6c838cf0adb3e04c94ca5e
Rukundo725 <gerukundo14@gmail.com>
2022-07-13 11:43:53 +0200

    rebased and solved conflicts

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import server from '../index';
-=======
 import assert from 'assert';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../app';
->>>>>>> develop
 chai.should();
 
 chai.use(chaiHttp);
 describe('MAIN PAGE', () => {
-<<<<<<< HEAD
-  it('it should return status 200', () => {(done) => {
-    chai
-        .request(server)
-        .get('/api/v1/')
-        .end((error, response) => {
-          chai.expect(response.statusCode).to.equal(200);
-        });
-    done();
-  }});
-=======
   it('it should return status 200', () => {
     (done) => {
       chai
@@ -34,5 +17,4 @@ describe('MAIN PAGE', () => {
       done();
     };
   });
->>>>>>> develop
 });
