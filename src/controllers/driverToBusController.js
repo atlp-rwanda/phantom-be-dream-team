65494d3265d43dba85bba833f7d7f736bd866eec
GYSSAGARA Prince <91846911+gyssa-prince@users.noreply.github.com>
2022-07-14 21:17:53 +0200

    Update driverToBusController.js

--- src/controllers/driverToBusController.js
+++ src/controllers/driverToBusController.js
@@ -13,7 +13,7 @@ const AssignDriverToBus = async (req, res) => {
         status: "fail",
         message: "The Driver found with that ID",
       });
-    } else if (user.role !== "driver") {
+    } else if (user.role !== "Driver") {
       return res.status(403).json({
         status: "fail",
         message: "The user is not a Driver, try again",
