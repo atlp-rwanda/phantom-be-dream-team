d0b3bf90a2ad341c5ddf9d20d8cbdefed5c86ec2
Rukundo725 <gerukundo14@gmail.com>
2022-07-14 22:51:01 +0200

    protected roles api

--- src/routes/api/roles.routes.js
+++ src/routes/api/roles.routes.js
@@ -1,15 +1,16 @@
 
 import express from 'express'
 import { createRole, allRoles, getRole, updateRole, deleteRole } from '../../controllers/rolescontrollers'
+import { checkAdmin } from '../../middleware/check'
 
 const router = express.Router()
 
 
-router.post('/', createRole)
-router.get('/', allRoles)
-router.get('/:Id', getRole)
-router.delete('/:Id', deleteRole)
-router.patch('/:Id', updateRole)
+router.post('/', checkAdmin,createRole)
+router.get('/', checkAdmin,allRoles)
+router.get('/:Id', checkAdmin,getRole)
+router.delete('/:Id',checkAdmin, deleteRole)
+router.patch('/:Id',checkAdmin, updateRole)
 
 
 export default router
