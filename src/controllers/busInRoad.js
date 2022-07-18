1fb7114d60890372f3667e983e1bb1bf9e89e0ea
GYSSAGARA Prince <andelarwanda@Andelas-MacBook-Pro.local>
2022-07-18 11:11:49 +0200

    Update bus info

--- src/controllers/busInRoad.js
+++ src/controllers/busInRoad.js
@@ -85,8 +85,8 @@ const  getBusInRoad =  async (req, res) => {
 
 
   const startBus =async (req, res) =>{
-    const {bus_Id,time_Start,speed,driver_Email,passangers,current_Loc,route_Id}=req.body;
-    const Time_updated=0,id=Math.round(Math.random()*100000);
+    const {bus_Id,time_Start,speed,driver_Email,passangers,current_Loc,route_Id,id}=req.body;
+    const Time_updated=0;
     if (bus_Id === '' || time_Start === '' || speed === '' || driver_Email === '' || route_Id==='') {
       return res.status(500).json({
         message: req.t('required_field'),
@@ -138,40 +138,33 @@ try{
 
   const UpdateBusInfo = async (req, res) =>{
 
-        const { speed, passangers, current_Loc} = req.body;
-        const { id } = req.params;
-        try {
-        const aBus = await busInRoad.findOne({
-          where: {
-            id,
-          },
-        });
-      
-        if (!aBus) {
-          return res.status(400).send({
-            message: 'There is no such ongoing bus'+' '+id
+    const { speed, passangers, current_Loc, Time_updated } = req.body;
+    const { id } = req.params;
+  
+    try {
+        const buses = await busInRoad.findOne({
+            where: {
+              id:id
+            },attributes: ['id','bus_Id','time_Start','speed','driver_Email','passangers','current_Loc','route_Id']
           });
-        }
-      
-        
-          if (speed) {
-            busInRoad.speed = speed;
-          }
-          if (passangers) {
-            busInRoad .passangers = passangers;
-          }
-          if (current_Loc) {
-              busInRoad .current_Loc = current_Loc;
+          if (buses=='') {
+                  return res.status(400).send({
+                    message: `There is no ongoing  bus with this `+id
+                  });
+                }else{
+                  buses.speed = speed,
+                  buses.passangers = passangers,
+                  buses.current_Loc = current_Loc
+                  buses.Time_updated = Time_updated
+                  buses.save();
+                  return res.status(200).send({
+                    message: `Bus info updated`+id
+                  });
+                }   } catch (err) {
+                  return res.status(500).send({
+                    message: `Error: ${err.message}`,
+                  });
+                }
+            
             }
-            busInRoad.Time_updated = '1656590753032'
-             aBus.save()
-             return res.status(200).send({
-              message: `Updated`,
-            });
-        } catch (err) {
-          return res.status(500).send({
-            message: `Error: ${err.message}`,
-          });
-        }
-      };
   export { startBus, getBusInRoad, SpecificBusinRoad, getAllBusInRoad, UpdateBusInfo, stopBus};
